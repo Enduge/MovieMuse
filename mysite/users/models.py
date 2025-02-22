b14438fb6c7e0bb3f21d37c3a29656faf02203b0
Enduge <jacobnelson@mail.adelphi.edu>
2025-02-21 19:23:32 -0500

    Update models.py

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -4,7 +4,7 @@ from django_resized import ResizedImageField
 
 class Profile(models.Model, ResizedImageField):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = ResizedImageField(size=[400, 400], default='default.png', upload_to='profile_images')
+    avatar = ResizedImageField(size=[250, 250], default='default.png', upload_to='profile_images')
 
     def __str__(self):
         return self.user.username
